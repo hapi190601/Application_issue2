09a08591ae427d98a9d102fa53766762bc11e14c
hapi190601 <gc1206ro@gmail.com>
2021-03-02 06:53:48 +0000

    [1550]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = current_user
   end
 
   def index
@@ -11,18 +13,25 @@ class BooksController < ApplicationController
   end
 
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    @user = User.find(current_user.id)
     if @book.save
-      redirect_to book_path(@book), notice: "You have created book successfully."
+      redirect_to book_path(@book.id)
+      flash[:notice] = "You have created book successfully."
     else
-      @books = Book.all
       render 'index'
     end
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render edit
+    else
+      redirect_to books_path
+    end
   end
 
 
@@ -36,16 +45,16 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :books
   attachment :profile_image, destroy: false
 
-  validates :name, length: {maximum: 20, minimum: 2}
-  validates :name, presence: true
+  validates :name, length: {maximum: 20, minimum: 2}, presence: true
+  validates :introduction, length: {maximum: 20}
 end
